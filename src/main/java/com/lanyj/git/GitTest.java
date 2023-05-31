fe0b0343c66a3d42b29b0165e54489c2e99c463c
lan <lanyingjian0904@163.com>
2023-05-31 14:47:00 +0800

    second commit

--- src/main/java/com/lanyj/git/GitTest.java
+++ src/main/java/com/lanyj/git/GitTest.java
@@ -3,5 +3,8 @@ package com.lanyj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,lanyj");
+        System.out.println("hello,yao");
+        System.out.println("hello,xie");
     }
 }
